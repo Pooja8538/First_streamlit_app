73fb451302db92751bcf6d45a7d180e91e4e216e
Pooja8538 <114646240+Pooja8538@users.noreply.github.com>
2022-11-08 11:44:40 +0530

    Update streamlit_app.py
    added multiselect to line
    added pandas dataframe loc

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,8 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits :" ,list(my_fruit_list.index),['Apple' , 'Banana'])
+
+fruits_selected = streamlit.multiselect("Pick some fruits" , list(my_fruit_list.index),['Apple' , 'Banana'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
